5dcafe6c902d7ff430d0f3c46968a37f49830c3e
metabit1000 <aslexag0@gmail.com>
2020-05-05 21:34:30 +0200

    FlagShip1, falta probar disparos disparos diferentes

---
+++ Assets/Scripts/Controllers/BigTurretBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigTurretBehaviour : MonoBehaviour
+{
+    AttackComponent attackComponent;
+    Vector3 playerPosition;
+    Transform cannon;
+
+    Transform transformPlayer;
+    
+
+    void Start()
+    {
+    	attackComponent = this.GetComponent<AttackComponent>();
+    	cannon = transform.GetChild(0);
+        transformPlayer = GameObject.FindGameObjectWithTag("Player").transform; 
+    }
+    
+    //TODO: call findPlayer only once in start
+    void Update()
+    {
+        if (transformPlayer != null) 
+        {
+            playerPosition = transformPlayer.position;
+            LookAt(playerPosition);
+        } 
+		
+		if (attackComponent != null)
+        {
+            attackComponent.BasicAttack();
+        }
+
+    }
+
+    public void LookAt(Vector3 target)
+    {
+    	Vector3 targetPosition = new Vector3(target.x,transform.position.y,target.z); //rotate in Y-axis
+    	transform.LookAt(targetPosition);
+    	cannon.LookAt(target);
+    }
+}
