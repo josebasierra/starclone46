8ef0ca37649a166b71f577a360473cb87185a1ac
metabit1000 <aslexag0@gmail.com>
2020-05-07 20:39:41 +0200

    Pause Menu

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -5,17 +5,41 @@ using UnityEngine;
 public class PauseManager : MonoBehaviour
 {
     bool isPaused = false;
+    GameObject player;
+
+    public GameObject pauseMenu;
 
     public bool IsPaused() => isPaused;
     public void SetIsPaused(bool value) => isPaused = value;
+
+    void Start() 
+    {
+        player = GameObject.FindGameObjectWithTag("Player"); //para desactivar el script "Crosshair"
+    }
 
     void Update()
     {
         if (Input.GetKeyDown("escape"))
         {
+            if (isPaused) Resume(); 
+            else Pause();
+
             isPaused = !isPaused;
-            Time.timeScale = (Time.timeScale + 1) % 2;
         }
 
+    }
+
+    void Resume() 
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        player.GetComponent<Crosshair>().enabled = true;
+    }
+
+    void Pause() 
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        player.GetComponent<Crosshair>().enabled = false;
     }
 }
